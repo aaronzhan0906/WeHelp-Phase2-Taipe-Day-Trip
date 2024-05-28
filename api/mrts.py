707905065451fd08e62777bcca62a280d50242bb
ChengTing <aaronzhan0906@gmail.com>
2024-05-28 13:39:41 +0800

    mrts_fix_20240528

--- api/mrts.py
+++ api/mrts.py
@@ -4,19 +4,16 @@ from data.database import get_cursor, commit_changes
 
 router = APIRouter()
 
-# link database.py
-
 @router.get("/mrts")
 async def attractions(request: Request):
 
-    # SELECT MRT 出現在 table 裡的統計數量
     try:
         cursor, conn = get_cursor()
         cursor.execute("SELECT mrt, COUNT(*) AS count FROM attractions GROUP BY mrt ORDER BY count DESC;")
         sorted_mrts = cursor.fetchall()
         commit_changes(conn)
 
-        sorted_mrts_names = [mrt for mrt, _ in sorted_mrts ]
+        sorted_mrts_names = [mrt for mrt, _ in sorted_mrts if mrt is not None]
         return JSONResponse(content={"data":sorted_mrts_names})
     except Exception as exception:
         return JSONResponse(content={"error": True, "message": str(exception)})
